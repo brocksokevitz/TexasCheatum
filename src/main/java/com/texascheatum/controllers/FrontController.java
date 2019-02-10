179e994971eb28930bf6576623819be1fe619a96
NathanLosee <nathan.d.losee@gmail.com>
2019-02-10 12:26:11 -0500

    added logging

--- src/main/java/com/texascheatum/controllers/FrontController.java
+++ src/main/java/com/texascheatum/controllers/FrontController.java
@@ -9,9 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.servlets.DefaultServlet;
+import org.apache.log4j.Logger;
 
 public class FrontController extends DefaultServlet {
 	private static final long serialVersionUID = -8870267947467247584L;
+	private static final Logger log = Logger.getLogger(FrontController.class);
+	
 	private static List<String> fileExts = new ArrayList<>();
 	
 	@Override
@@ -26,8 +29,9 @@ public class FrontController extends DefaultServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		String uri = request.getRequestURI();
+		log.info(request.getMethod() + " " + uri);
 		
-		if (uri.equals("/TexasCheatum/") || uri.equals("/TexasCheatum/"))
+		if (uri.equals("/TexasCheatum/") || uri.equals("/TexasCheatum"))
 			request.getRequestDispatcher("/TexasCheatum/ng/").forward(request, response);
 		else if (uri.equals("/TexasCheatum/ng/") ||
 				(uri.lastIndexOf('.') > -1 && fileExts.contains(uri.substring(uri.lastIndexOf('.')))))
