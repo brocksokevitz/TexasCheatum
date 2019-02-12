2b09fcf974dd2212304c54dcbf8baf2837bfa341
Marcellus Traynor <marceltraynor@live.com>
2019-02-12 15:57:26 -0500

    Created menu flow outline with routing

--- src/main/ngapp/src/app/app.module.ts
+++ src/main/ngapp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableContainerComponent } from './components/table-container/table-container.component';
+import { LoginComponent } from './components/login/login.component';
+import { GameSettingsComponent } from './components/game-settings/game-settings.component';
+import { HomeComponent } from './components/home/home.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { OptionsMenuComponent } from './components/options-menu/options-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableContainerComponent,
+    LoginComponent,
+    GameSettingsComponent,
+    HomeComponent,
+    RulesComponent,
+    OptionsMenuComponent
   ],
   imports: [
     BrowserModule,
