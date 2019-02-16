3b8fad973fb7d894319a036471f6fc9185838738
Marcellus Traynor <marceltraynor@live.com>
2019-02-16 17:57:50 -0500

    Switched stuff back to JSON

--- src/main/ngapp/src/app/services/game.service.ts
+++ src/main/ngapp/src/app/services/game.service.ts
@@ -10,22 +10,22 @@ export class GameService {
 
   getHand() {
     console.log("inside the getHand method of game.service");
-    return this.http.get<any>("/TexasCheatum/servlet/getHand", {responseType: "text" as "json"});
+    return this.http.get<any>("/TexasCheatum/servlet/getHand");
   }
 
   getFlop() {
     console.log("inside the getFlop method of game.service");
-    return this.http.get<any>("/TexasCheatum/servlet/getFlop", {responseType: "text" as "json"});
+    return this.http.get<any>("/TexasCheatum/servlet/getFlop");
   }
 
   getTurn() {
     console.log("inside the getTurn method of game.service");
-    return this.http.get<any>("/TexasCheatum/servlet/getTurn", {responseType: "text" as "json"});
+    return this.http.get<any>("/TexasCheatum/servlet/getTurn");
   }
 
   getRiver() {
     console.log("inside the getRiver method of game.service");
-    return this.http.get<any>("/TexasCheatum/servlet/getRiver", {responseType: "text" as "json"});
+    return this.http.get<any>("/TexasCheatum/servlet/getRiver");
   }
 
   cheat() {
