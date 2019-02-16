9f6c0e87acedece1f420e72773cb7f2c17a24f1e
Brock Sokevitz <bsokevitz@gmail.com>
2019-02-16 18:34:11 -0500

    get hand score
    hand score done

--- src/main/java/com/texascheatum/services/GameService.java
+++ src/main/java/com/texascheatum/services/GameService.java
@@ -25,12 +25,24 @@ public class GameService {
 				gameJson.get("username").asText());
 	}
 	
-	public static int checkHand(String[] hand) {
+	
+	/**
+	 * 
+	 * @param hand
+	 * highest hand type wins
+	 * highest card only comes into play for ties
+	 * highest pair is used for pair, two pair, and fullhouse(in case of highest card tie)
+	 * lowest pair is only used for two pair ties
+	 * @return int[] highestScore = {handType, highestCard, highest pair, lowest pair}
+	 */
+	public static int[] checkHand(String[] hand) {
 		
-		int highestScore = 0;
-		int currentScore = 0;
+		int[] highestScore = {0,0,0,0};
+		int[] currentScore = {0,0,0,0};
 		int[] handHistogram = new int[13];
+		int[] suitHistogram = new int[4];
 		boolean flush = false;
+		boolean straight = false;
 		
 		if(hand.length >5) {
 			//remove random element and send it through check hand again until it meets hand size
@@ -45,9 +57,25 @@ public class GameService {
 				
 				currentScore = checkHand(newHand);
 				
-				if(currentScore>highestScore) {
+				if(currentScore[0]>=highestScore[0]) {
 					
-					highestScore=currentScore;
+					//change the highest card
+					if(currentScore[0]>highestScore[0] ||  currentScore[1]>highestScore[1]) {
+					highestScore[1]=currentScore[1];		
+					}
+					
+					//change the hand type
+						highestScore[0]=currentScore[0];
+					
+					//only used for pairs and 2 pairs for highest pair
+						if(currentScore[2]>highestScore[2]) {
+							highestScore[2]=currentScore[2];		
+							}				
+					
+					//only used for the lowest pair for 2 pair
+						if(currentScore[3]>highestScore[3]) {
+							highestScore[3]=currentScore[3];		
+							}
 					
 				}
 			} //end of outer for loop
@@ -60,12 +88,13 @@ public class GameService {
 				if(Character.isDigit(hand[k].charAt(0))) {
 					//every digit except 0 can be placed based on their value into the histogram
 					//0 is technically 10, so it needs to be handled specially for the histogram
-					if(Character.getNumericValue(hand[k].charAt(0))>0) {
+					if(Character.getNumericValue(hand[k].charAt(0)) > 0) {
 						
 						handHistogram[Character.getNumericValue(hand[k].charAt(0))-1]++;
 						
 					}else {
 						handHistogram[9]++;
+						
 					}
 					
 				}else { //if it's not a number it's an Ace, Jack, Queen, or King
@@ -90,32 +119,111 @@ public class GameService {
 					
 				}
 				
+			} //end of for
+			
+			//find highest card for draws and straights
+			for(int z = handHistogram.length; z > 0; z--) {
+				if(handHistogram[z%handHistogram.length]==1 && z+1>highestScore[1]) {
+					highestScore[1] = z+1;
+				}
+			}
+			//suit histogram for flushes
+			for(int k = 0; k<hand.length; k++) {
+				switch(hand[k].charAt(1)) {
+				case 'D':
+					suitHistogram[0]++;
+					break;
+				case 'H':
+					suitHistogram[1]++;
+					break;
+				case 'C':
+					suitHistogram[2]++;
+					break;
+				case 'S':
+					suitHistogram[3]++;
+					break;
+				default:
+					break;
+				
+				}
 			}
 			//System.out.println(Arrays.toString(handHistogram));
 		}
 		
 		if(ArrayUtils.contains(handHistogram, 4)) {
 			
-			
+			highestScore[0]=7;			
 			
 		}else if(ArrayUtils.contains(handHistogram, 3) && ArrayUtils.contains(handHistogram, 2)) {
 			
+			highestScore[0]=6;
+			highestScore[1]=ArrayUtils.indexOf(handHistogram, 3)+1;
+			highestScore[2]=ArrayUtils.indexOf(handHistogram, 2)+1;
+			
 		}else if(ArrayUtils.contains(handHistogram, 3) && ArrayUtils.contains(handHistogram, 1)) {
 			
+			highestScore[0]=3;
+			
 		}else if(ArrayUtils.contains(handHistogram, 2) && !ArrayUtils.contains(handHistogram, 3)) {
+			
 			int count = 0;
 			int highestPair=-7;
 			int lowestPair=-5;
-			for(int m = 0; m<handHistogram.length;m++ ) {
-				if(handHistogram[m]==2) {
+			
+			for(int m = handHistogram.length; m>0 ;m--) {
+				if(handHistogram[m%handHistogram.length]==2) {
 					count++;
 					if(count==1) {
-						highestPair=m;
-					}else{
-						lowestPair=highestPair;
-						highestPair=m;
+						highestPair=m+1;
+					}else if(count==2){
+						lowestPair=m+1;
 					}
 				}
+			}
+			
+			if(count==2) {
+				highestScore[0]=2;
+				highestScore[2]=highestPair;
+				highestScore[3]=lowestPair;
+			}else {
+				highestScore[0]=1;
+				highestScore[2]=highestPair;
+			}
+			
+		}else {
+			
+			//if there are no multiples, then it could be a flush
+			if(ArrayUtils.contains(suitHistogram, 5)) {
+				
+				flush=true;
+				highestScore[0]=5;
+				
+			}	
+			
+			//and/or a straight
+			for(int y = 0; y < handHistogram.length-3; y++) {
+				
+				if(handHistogram[y]==1 && handHistogram[y+1]==1 && handHistogram[y+2]==1 
+						&& handHistogram[y+3]==1 && handHistogram[(y+4)%handHistogram.length]==1) {
+					
+					if(flush && y==9){
+						
+						highestScore[0]=9;	
+						
+					}else if(flush) {
+						
+						highestScore[0]=8;
+						highestScore[1]=y+5;
+						
+					}else if(highestScore[0] <= 4){
+						
+						highestScore[0]=4;
+						highestScore[1]=y+5;
+						
+					}
+					
+				}
+				
 			}
 			
 		}
--- src/test/java/com/texascheatum/test/UserDaoTest.java
+++ src/test/java/com/texascheatum/test/UserDaoTest.java
@@ -34,9 +34,91 @@ public class UserDaoTest {
 	}
 	
 	@Test
-	public void testHistogram() {
-		String[] hand = {"AC","2C","3C","4S","5H","6H","7H"};
-		int score = GameService.checkHand(hand);
-		Assert.assertEquals(0,score);
+	public void testNothing() {
+		String[] hand = {"AC","2C","4C","9S","5H","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(0,score[0]);
+		Assert.assertEquals(14,score[1]);
+	}
+	
+	@Test
+	public void testSinglePair() {
+		String[] hand = {"AC","2C","4C","4S","5H","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(1,score[0]);
+	}
+	
+	@Test
+	public void testTwoPair() {
+		String[] hand = {"2S","2C","4C","4S","5H","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(2,score[0]);
+		Assert.assertEquals(7,score[1]);
+		Assert.assertEquals(4,score[2]);
+		Assert.assertEquals(2,score[3]);
+	}
+	
+	@Test
+	public void testThreeOfAkind() {
+		String[] hand = {"AC","4H","4C","4S","5H","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(3,score[0]);
+		Assert.assertEquals(14,score[1]);
+	}
+	
+	@Test
+	public void testStraight() {
+		String[] hand = {"AH","2C","3H","4H","5C","6S","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(4,score[0]);
+		Assert.assertEquals(7,score[1]);
+	}
+	
+	@Test
+	public void testFlush() {
+		String[] hand = {"AH","4H","5H","KH","9C","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(5,score[0]);
+		Assert.assertEquals(14,score[1]);
+	}
+	
+	@Test
+	public void testFullHouse() {
+		String[] hand = {"KC","4H","4C","4S","KH","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(6,score[0]);
+		Assert.assertEquals(4,score[1]);
+		Assert.assertEquals(13,score[2]);
+	}
+	
+	@Test
+	public void testFourOfAKind() {
+		String[] hand = {"4D","4H","4C","4S","5H","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(7,score[0]);
+		Assert.assertEquals(7,score[1]);
+	}
+	
+	@Test
+	public void testStraightFlush() {
+		String[] hand = {"AH","2H","3H","4H","5H","6H","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(8,score[0]);
+		Assert.assertEquals(7,score[1]);
+	}
+	
+	@Test
+	public void testLowerStraightFlush() {
+		String[] hand = {"AH","2H","3H","4H","5H","6S","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(8,score[0]);
+		Assert.assertEquals(5,score[1]);
+	}
+	
+	@Test
+	public void testRoyalFlush() {
+		String[] hand = {"0H","KH","JH","QH","AH","6S","7H"};
+		int[] score = GameService.checkHand(hand);
+		Assert.assertEquals(9,score[0]);
 	}
 }
