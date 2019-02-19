577e716cd0ccb940e25857b729726b5031df9e81
Brock Sokevitz <bsokevitz@gmail.com>
2019-02-19 15:39:37 -0500

    pool with static block

--- src/main/java/com/texascheatum/utils/TomcatConnectionPool.java
+++ src/main/java/com/texascheatum/utils/TomcatConnectionPool.java
@@ -16,20 +16,21 @@ public class TomcatConnectionPool {
 
 	private static DataSource dataSource;
 
-	private TomcatConnectionPool() {
-		super();
+	static {
+
 		try {
 			Context initialContext = new InitialContext();
 			Context environmentContext = (Context) initialContext.lookup("java:comp/env");
 			dataSource = (DataSource) environmentContext.lookup("jdbc/TexasCheatum");
 		} catch (NamingException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error(e.getMessage());
 		}
 		
 	}
 	
 	public static Connection getConnection(){
+		
 			try {
 				return dataSource.getConnection();
 			} catch (SQLException e) {
