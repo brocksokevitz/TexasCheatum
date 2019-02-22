2cc81c92fe24d65654c1e7e4a3e90d8164112d48
Marcellus Traynor <marceltraynor@live.com>
2019-02-21 21:07:47 -0500

    Formatting and rules

--- src/main/ngapp/src/app/components/rules/rules.component.ts
+++ src/main/ngapp/src/app/components/rules/rules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TableContainerComponent } from '../table-container/table-container.component';
 
 @Component({
   selector: 'app-rules',
@@ -7,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RulesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private table: TableContainerComponent
+  ) { }
 
   ngOnInit() {
   }
 
   goBack() {
     window.history.back();
+    this.table.resume();
   }
 }
