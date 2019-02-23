eb6da4eebfe51c90482f8e7476fe0567c128a0ba
Marcellus Traynor <marceltraynor@live.com>
2019-02-22 20:50:22 -0500

    Keeping track of the monies

--- src/main/ngapp/src/app/components/gameplay/gameplay.component.ts
+++ src/main/ngapp/src/app/components/gameplay/gameplay.component.ts
@@ -6,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gameplay.component.css']
 })
 export class GameplayComponent implements OnInit {
-  
+  private money: any = {};
+  private totalBets: number;
 
   constructor() {}
 
   ngOnInit() {
   }
 
+  bet() {
+    this.totalBets += this.money.bet;
+    console.log(this.totalBets);
+  }
+
+  call() {
+    this.totalBets += this.money.call;
+    console.log(this.totalBets);
+  }
+
+  raise() {
+    this.totalBets += this.money.raise;
+    console.log(this.totalBets);
+  }
 }
