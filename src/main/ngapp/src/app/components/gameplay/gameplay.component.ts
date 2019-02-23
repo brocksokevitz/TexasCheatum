7a31966b49407b0e879e6c1b938f62b5de5e7231
Marcellus Traynor <marceltraynor@live.com>
2019-02-22 21:33:21 -0500

    Bed time push 3

--- src/main/ngapp/src/app/components/gameplay/gameplay.component.ts
+++ src/main/ngapp/src/app/components/gameplay/gameplay.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gameplay.component.css']
 })
 export class GameplayComponent implements OnInit {
-  private betAmount: number;
-  private callAmount: number;
-  private raiseAmount: number;
-  private totalAmount: number;
+  private betAmount: number = null;
+  private callAmount: number = null;
+  private raiseAmount: number = null;
+  private totalAmount: number = null;
 
   constructor() {}
 
@@ -17,17 +17,26 @@ export class GameplayComponent implements OnInit {
   }
 
   betting() {
+    console.log(this.totalAmount);
+    console.log(this.betAmount);
     this.totalAmount += this.betAmount;
     console.log(this.totalAmount);
+    this.betAmount = null;
   }
 
   calling() {
+    console.log(this.totalAmount);
+    console.log(this.callAmount);
     this.totalAmount += this.callAmount;
     console.log(this.totalAmount);
+    this.callAmount = null;
   }
 
   raising() {
+    console.log(this.totalAmount);
+    console.log(this.raiseAmount);
     this.totalAmount += this.raiseAmount;
     console.log(this.totalAmount);
+    this.raiseAmount = null;
   }
 }
