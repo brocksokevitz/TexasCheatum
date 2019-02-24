726aceb160647eae74dafc20b6cb49709345c5c3
Marcellus Traynor <marceltraynor@live.com>
2019-02-23 19:40:51 -0500

    Display whos turn it is

--- src/main/ngapp/src/app/components/gameplay/gameplay.component.ts
+++ src/main/ngapp/src/app/components/gameplay/gameplay.component.ts
@@ -31,6 +31,7 @@ export class GameplayComponent implements OnInit {
               this.status.pot = data.pot;
               this.status.balance = data.balance;
               this.status.minimum = data.minimum;
+              this.status.turn = data.turn;
               this.status.hand = data.hand;
               this.status.table = data.table;
               this.tableCards = this.status.table;
