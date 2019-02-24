a3c5d700e7b3362540a7afc5b95cd7865db26536
Brock Sokevitz <bsokevitz@gmail.com>
2019-02-24 12:03:03 -0500

    added email for register
    lol

--- src/main/java/com/texascheatum/services/PlayerService.java
+++ src/main/java/com/texascheatum/services/PlayerService.java
@@ -1,10 +1,21 @@
 package com.texascheatum.services;
 
 import java.io.IOException;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +24,9 @@ import com.texascheatum.model.AbstractUser;
 import com.texascheatum.model.User;
 
 public class PlayerService {
+	
+	final static Logger log = Logger.getLogger(PlayerService.class);
+	
 	private PlayerService() { }
 	private static final ObjectMapper mapper = new ObjectMapper();
 	
@@ -21,6 +35,10 @@ public class PlayerService {
 		JsonNode userJson = mapper.readTree(request.getReader());
 		UserDaoImplementation.getUserDao().insertUser(
 				userJson.get("username").asText(),
+				userJson.get("email").asText(),
+				userJson.get("password").asText());
+		
+		sendEmail(userJson.get("username").asText(),
 				userJson.get("email").asText(),
 				userJson.get("password").asText());
 	}
@@ -53,5 +71,57 @@ public class PlayerService {
 		request.getSession().invalidate();
 //		request.logout();
 		response.setHeader("status", "200");
+	}
+	
+	public static boolean sendEmail(String username,String password,String email) {
+		
+		final String senderEmail = "erstestemail7@gmail.com";// change with your sender email
+		final String senderPassword = "p4ssw0rd.";// change with your sender password
+		
+		String title = "new user";
+		
+		String to = email;
+		
+		String body = "<!DOCTYPE html>" + 
+				"<html>" + 
+				"<body>"
+				+ "<p>Username: "+username+"<br>Password: "+password+"<br><br>Please have fun playing poker at <a href=\"http://ec2-18-218-85-44.us-east-2.compute.amazonaws.com:8080/TexasCheatum/\">Texas Cheatum</a>!"
+						+ "<b>This email was formatted using HTML.</p>"
+						+ "</body>"
+						+ "</html>";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(senderEmail, senderPassword);
+			}
+		});
+
+		try {
+			// create message using session
+			MimeMessage message = new MimeMessage(session);
+			message.setContent(body, "text/html; charset=utf-8");
+			message.saveChanges();
+			message.setFrom(new InternetAddress(senderEmail));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setSubject(title);
+			// sending message
+			Transport.send(message);
+			return true;
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			log.error(e.getMessage());
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			log.error(e.getMessage());
+		} catch(NullPointerException e) {
+			log.error(e.getMessage());
+		}
+		return false;
 	}
 }
